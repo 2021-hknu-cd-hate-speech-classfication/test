1eaa25fc0e87ea87a475d61b0fe4da03fcfe967a
vallisneria <moogi3469@gmail.com>
2021-07-06 20:58:34 +0900

    setup.py 일부 수정

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
-from setuptools import setup
+import setuptools
 
-with open("requirements.txt") as file:
+with open("README.md", mode="r", encoding="utf-8") as file:
+    readme_md = file.read().splitlines()
+
+with open("requirements.txt", mode="r", encoding="utf-8") as file:
     dependency_list = file.read().splitlines()
 
-setup(
+setuptools.setup(
     name="python_pip_test",
     version="0.0.1",
+    description="테스트",
+    long_description=readme_md,
+    long_description_content_type="text/markdown",
+    url="https://github.com/2021-hknu-cd-hate-speech-classfication/test",
     install_requires=dependency_list
 )
